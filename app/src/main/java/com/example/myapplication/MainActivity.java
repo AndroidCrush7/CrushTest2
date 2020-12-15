31ba6e6cb5fdc1ff9ad63cfb83741f1df8119f9b
Crush <2117104454@qq.com>
2020-12-15 08:47:25 +0800

    哈哈哈

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String string="你hhh好";
+        String string="你hhh77好";
     }
 }
